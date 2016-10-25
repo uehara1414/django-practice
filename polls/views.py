873caee9ff7da94536ae530b6e034bd8fffa361d
uehara1414 <b1015062@fun.ac.jp>
2016-10-25 16:51:35 +0900

    ショートカットの方法を間違えていたので修正

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,7 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list
     }
-    return render(request, render("polls/index.html", context))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
