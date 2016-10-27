dfec5d1c1273bc6e172e773ce9b418275e2a031f
uehara1414 <b1015062@fun.ac.jp>
2016-10-27 14:41:38 +0900

    不要になったコードを削除した

--- polls/views.py
+++ polls/views.py
@@ -19,27 +19,10 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", context={"question": question})
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
